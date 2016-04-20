ebeb211a00bd57b48aae930914efdfb3fedba019
Sean Rhoades <sean@seanrhoades.com>
2016-04-20 15:23:51 -0400

    working counter

---
+++ app/app.component.ts
@@ -0,0 +1,19 @@
+import {Component} from 'angular2/core';
+
+@Component({
+    selector: 'my-app',
+    template: `
+<h1>Hello {{title}}</h1>
+<span>Clicks: {{count}}<span>
+<button (click)="increaseCount()">Click me</button>
+`
+})
+
+export class AppComponent {
+    count = 0;
+    title: "Hello World";
+
+    increaseCount(){
+    this.count++;
+  }
+}
